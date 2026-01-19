63ba943a237e7c256c39881d13f27e8c39860771
Pavel Grishanin <pgrishanin@gmail.com>
2026-01-19 21:43:34 +0300

    vite build env fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-    base: process.env.VITE_PUBLIC_BASE,
-    plugins: [react(), tsconfigPaths()],
-    css: {
-        preprocessorOptions: {
-            less: {
-                math: 'always',
-                relativeUrls: true,
-                javascriptEnabled: true,
+export default defineConfig(({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    return {
+        base: process.env.VITE_PUBLIC_BASE,
+        plugins: [react(), tsconfigPaths()],
+        css: {
+            preprocessorOptions: {
+                less: {
+                    math: 'always',
+                    relativeUrls: true,
+                    javascriptEnabled: true,
+                },
             },
         },
-    },
-}));
+    };
+});
